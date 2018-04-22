12a0c3bd2560797ffbf649971e7b1ecf4385811e
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2018-04-22 23:53:13 +0900

    first commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import javax.inject._
+import models.DBAccess
+import play.api._
+import play.api.mvc._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(db: DBAccess) extends Controller {
+
+  /**
+   * Create an Action to render an HTML page with a welcome message.
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def insert(name: String, pass: String, admin: Boolean) = Action {
+    Ok(db.insert(name, pass, admin).toString)
+  }
+
+}
